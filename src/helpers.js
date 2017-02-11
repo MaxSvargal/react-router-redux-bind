e79ddb8d651e07fe0fc95ce297e60afc2546ec37
MaxSvargal <maxsvargal@gmail.com>
2017-02-11 14:58:31 +0300

    Fix type error in getRouteMatch

--- src/helpers.js
+++ src/helpers.js
@@ -3,4 +3,4 @@ import { matchPath } from 'react-router'
 export const getRouteMatch = (routes, pathname) =>
   !routes ? null : routes
     .map(r => matchPath(pathname, r.path, { exact: true }))
-    .filter(r => r !== null)[0] || nulle
+    .filter(r => r !== null)[0] || null
