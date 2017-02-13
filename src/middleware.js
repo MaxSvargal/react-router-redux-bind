da95491fa8ec75c03c14c95a1b7cba2da2e0d7bb
MaxSvargal <maxsvargal@gmail.com>
2017-02-13 12:52:05 +0300

    Rewrite middleware to ES6 style

--- src/middleware.js
+++ src/middleware.js
@@ -2,8 +2,9 @@ import { ROUTER_ACTION } from './actions'
 
 const routerMiddleware = history =>
   () => next => action => {
-    if (action.type !== ROUTER_ACTION) return next(action)
-    history[action.payload.method](...action.payload.args)
+    const { type, payload: { method, args } } = action
+    if (type !== ROUTER_ACTION) return next(action)
+    history[method](...args)
   }
 
 export default routerMiddleware
